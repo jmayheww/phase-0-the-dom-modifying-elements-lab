a5f6c65462634f9b14f964f8d676b7b780457337
Joshua <joshsmayhew@gmail.com>
2022-06-25 15:15:33 +0700

    commit tree 1ed24c4872b58daf8a06a010d91f7279e7bfb1d1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const selectBody = document.querySelector('body');
+const selectMain = document.getElementById('main');
+
+selectBody.removeChild(selectMain);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = " Josh is the champion ";
+
+selectBody.append(newHeader);
+
+
